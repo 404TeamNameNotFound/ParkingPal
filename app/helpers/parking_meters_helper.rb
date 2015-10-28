05ad47c51fa61844e9fa3db306257f4a7cfecb47
Sarah Yeo <sarahyeo2@gmail.com>
2015-10-27 18:53:08 -0700

    Impelemented parse_parking_meter method in parking_meter_helper.rb

--- app/helpers/parking_meters_helper.rb
+++ app/helpers/parking_meters_helper.rb
@@ -1,2 +1,38 @@
 module ParkingMetersHelper
+
+	def parse_parking_meters
+		url = 'http://data.vancouver.ca/download/kml/parking_meter_rates_and_time_limits.kmz'
+		zip_file = open(url)
+
+		unzip_folder = Zip::File.open(zip_file)
+		kml = unzip_folder.read('parking_meter_rates_and_time_limits.kml')
+
+		doc = Nokogiri::XML(kml)
+		doc.remove_namespaces!
+
+		places = doc.xpath('.//Placemark')
+		places.each do |meter|
+			temp_meter = ParkingMeter.new
+			meter.children.each do |node|
+				case node.node_name
+				when 'name'
+					temp_meter.name = node.text.to_i
+				when 'description'
+					parse_description(node, temp_meter)
+				when 'point'
+					parse_point(node, temp_meter)
+				end
+			end
+			temp_meter.save
+		end
+	end
+
+	def parse_description(node, temp_meter)
+		#STUB!!!
+	end
+
+	def parse_point(node, temp_meter)
+		#STUB!!!
+	end
+
 end
