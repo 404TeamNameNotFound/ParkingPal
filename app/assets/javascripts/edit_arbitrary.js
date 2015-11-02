3e60271c7b1998b167872cebc7bc2f4b5a5d5291
mparkms <mikeparkms@gmail.com>
2015-11-01 17:54:08 -0800

    Edit functionality complete

---
+++ app/assets/javascripts/edit_arbitrary.js
@@ -0,0 +1,55 @@
+$(function() {
+	$('#fetch').click(fetchMeter);
+	$('#submit').click(submit);
+
+	function fetchMeter() {
+		var id = $('#meter-id').val();
+		$.getJSON('/parking_meters/' + id + '.json', updateForm);
+	}
+
+	function updateForm(data) {
+		var startTime = parseTime(data.start_time);
+		var endTime = parseTime(data.end_time);
+		$('#parking_meter_id').val(data.id);
+		$('#parking_meter_name').val(data.name);
+		$('#parking_meter_price').val(data.price);
+		$('#parking_meter_max_time').val(data.max_time);
+		$('#parking_meter_start_time').val(startTime);
+		$('#parking_meter_end_time').val(endTime);
+		$('#parking_meter_is_broken').prop('checked', data.is_broken);
+		$('#parking_meter_is_occupied').prop('checked', data.is_occupied);
+	}
+
+	function parseTime(time) {
+		var timeArray = time.split('T')[1].split(':');
+		return timeArray[0] + ':' + timeArray[1];
+	}
+
+	function submit() {
+		var meterObject = createMeterObject();
+		$.ajax({
+			data: meterObject,
+			method: 'PATCH',
+			url: '/update_meter'
+		});
+	}
+
+	function createMeterObject() {
+		return {
+			id: parseInt($('#parking_meter_id').val()),
+			parking_meter: {
+				name: parseInt($('#parking_meter_name').val()),
+				price: parseInt($('#parking_meter_price').val()),
+				max_time: parseInt($('#parking_meter_max_time').val()),
+				start_time: toTime($('#parking_meter_start_time').val()),
+				end_time: toTime($('#parking_meter_end_time').val()),
+				is_broken: $('#parking_meter_is_broken').is(':checked'),
+				is_occupied: $('#parking_meter_is_occupied').is(':checked')
+			}
+		}
+	}
+
+	function toTime(time) {
+		return '2000-01-01T' + time + ':00.000Z';
+	}
+})
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
   #   get 'products/:id' => 'catalog#view'
 
   get    'update'  => 'parking_meters#parse'
+  get 'edit' => 'parking_meters#edit_arbitrary'
+  patch 'update_meter' => 'parking_meters#update'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
