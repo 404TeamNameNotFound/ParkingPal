984cbf658fd20c275ee827e909a0f07a77f3d543
Sydney <sydneycampbell4@gmail.com>
2015-11-13 15:26:45 -0800

    name search function in lat lon

--- app/models/lat_lon.rb
+++ app/models/lat_lon.rb
@@ -2,4 +2,9 @@ class LatLon < ActiveRecord::Base
 	validates :lat,:lon,  presence: true
 	validates :lat,:lon, numericality: true
 	belongs_to :parking_meter
+
+	def self.search(search)
+		where("parking_meter.name LIKE ?", "%{search}%")
+	end
+	
 end
--- app/models/parking_meter.rb
+++ app/models/parking_meter.rb
@@ -2,8 +2,4 @@ class ParkingMeter < ActiveRecord::Base
 	validates :name, :price, presence: true
 	has_one :lat_lon
 
-	def self.search(search)
-		where("name LIKE ?", "%{search}%")
-	end
-
 end
