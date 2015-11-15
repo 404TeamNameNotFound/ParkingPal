d81d6a11f24e3ef128e3355d7115c7488574504c
Sarah Yeo <sarahyeo2@gmail.com>
2015-11-14 20:03:10 -0800

    Skeleton search by location form

--- app/assets/javascripts/lat_lon.js
+++ app/assets/javascripts/lat_lon.js
@@ -12,7 +12,3 @@ function parseTime(time) {
 	var timeArray = time.split('T')[1].split(':');
 	return timeArray[0] + ':' + timeArray[1];
 }
-
-function makeListItem(marker) {
-	return '<a href="#" class="list-group-item"><b>Parking Meter: </b>' + marker.meter_name + '</a>';
-}
