7a3f89d5a7f1cf676bea1003e257214918a43176
mparkms <mikeparkms@gmail.com>
2015-11-16 20:56:36 -0800

    Details CSS tweaks, back button, click search results (not working)

--- app/assets/javascripts/lat_lon.js
+++ app/assets/javascripts/lat_lon.js
@@ -1,3 +1,12 @@
+$(function() {
+	$('#back-to-results').click(returnResults);
+	$('.list-group-item').click(function() {
+		var id = $(this).find('#results-meter-id').text();
+		console.log(id);
+		$.getJSON('/parking_meters/' + id + '.json', displayInfo);
+	})
+});
+
 function onMarkerClick(marker, event){
 	return function(event){
 		$.getJSON('/parking_meters/' + marker.meter_id + '.json', displayInfo);
@@ -11,8 +20,8 @@ function displayInfo(data) {
 	$('#meter-name').text(data.name);
 	$('#meter-price').text('$' + data.price);
 	$('#meter-max-time').text(data.max_time + ' hrs');
-	$('#meter-start-time').text(parseTime(data.start_time));
-	$('#meter.end-time').text(parseTime(data.end_time));
+	$('#meter-start-time').text(data.start_time);
+	$('#meter.end-time').text(data.end_time);
 
 	$('#meter-broken').toggleClass('label-success', !data.is_broken);
 	$('#meter-broken').toggleClass('label-danger', data.is_broken);
@@ -36,4 +45,9 @@ function displayInfo(data) {
 function parseTime(time) {
 	var timeArray = time.split('T')[1].split(':');
 	return timeArray[0] + ':' + timeArray[1];
+}
+
+function returnResults() {
+	$('#meter-details').hide();
+	$('#search-results').show();
 }
