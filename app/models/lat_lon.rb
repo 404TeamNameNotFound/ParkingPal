5d910ff34091a37844ea4837dd05fda65a91bc5e
Sydney <sydneycampbell4@gmail.com>
2015-11-22 17:04:01 -0800

    Deleted closest_parking and current_location methods in models

--- app/models/lat_lon.rb
+++ app/models/lat_lon.rb
@@ -1,22 +1,16 @@
 class LatLon < ActiveRecord::Base
+	acts_as_mappable :default_units => :meters,
+									 :default_formula => :flat,
+									 :distance_field_name => :distance,
+									 :lat_column_name => :lat,
+									 :lng_column_name => :lon
+
 	validates :lat,:lon,  presence: true
 	validates :lat,:lon, numericality: true
 	belongs_to :parking_meter
 
 	def self.search(search)
 		includes(:parking_meter).where("parking_meters.name LIKE ?", "%#{search}%").references(:parking_meters)
-	end
-
-	def self.closest_meter(location, radius)
-		geocoded_location = Geocoder.coordinates(location)
-		#includes(:parking_meter).near([geocoded_location], radius, :units => :km).references(:parking_meters)
-		LatLon.near([geocoded_location], radius, :units => :km)
-	end
-
-	def self.current_location(location, radius)
-		_latitude = request.location.latitude
-		_longitude = request.location.longitude
-		LatLon.near([_latitude, _longitude], radius, :units => :km)
 	end
 
 	def self.cheapest_meter
