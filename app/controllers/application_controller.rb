97e55afbf1a194fd299b07e7800498c50ab1fbc4
Vanessa <vanessa_wan93@hotmail.com>
2015-11-24 18:20:06 -0800

    visitors and users now can't navigate to admin restricted pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,26 +2,23 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
+  helper_method :require_admin
   helper_method :current_user
 
+
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+
   end
 
   def require_user
     redirect_to '/login' unless current_user
   end
 
-   def require_admin
-     redirect_to '/' unless current_user.admin?
-   end
 
-=begin
   def require_admin
-    flash[:error] == "You must be logged in to access this section"
-    redirect_to '/' unless current_user.admin?
+    redirect_to '/' unless current_user.try(:admin?)
   end
-=end
+
 end
 
