42b39dc331026845d4ee4ed59d1f86e9fb476ccd
mparkms <mikeparkms@gmail.com>
2015-11-25 22:45:49 -0800

    Seeds and fix modals not working

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 vanessa = User.create(first_name: 'Vanessa', last_name: 'Wan', email: 'vanessa@email.com', password: 'vanessa123', password_confirmation: 'vanessa123', role: 'admin')
+p = ParkedMeter.create(user: vanessa)
+vanessa.parked_meter = p
+vanessa.save
