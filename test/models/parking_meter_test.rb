7563f1b7dff0c19fb016dec4582213baacd57c23
Sarah Yeo <sarahyeo2@gmail.com>
2015-11-27 13:52:09 -0800

    Add parsing test for normal meter

--- test/models/parking_meter_test.rb
+++ test/models/parking_meter_test.rb
@@ -5,19 +5,30 @@ class ParkingMeterTest < ActiveSupport::TestCase
 
   test "parse skip missing args" do
     parse_test
-    assert_equal(10, ParkingMeter.count)
+    assert_equal(11, ParkingMeter.count)
+  end
+
+  test "parse normal" do
+    parse_test
+    assert_equal(11, ParkingMeter.count)
+    assert_equal(2, ParkingMeter.where(name: 12345).first.max_time)
+    assert_equal(3, ParkingMeter.where(name: 12345).first.price)
+    assert_equal(32400, ParkingMeter.where(name: 12345).first.start_time)
+    assert_equal(79200, ParkingMeter.where(name: 12345).first.end_time)
+    assert_equal(-123.106760403913, ParkingMeter.where(name: 12345).first.lat_lon.lon)
+    assert_equal(49.2827714869466, ParkingMeter.where(name: 12345).first.lat_lon.lat)
   end
 
   test "parse neg vaues" do
   	parse_test
-    assert_equal(10, ParkingMeter.count)
+    assert_equal(11, ParkingMeter.count)
     assert_equal(5, ParkingMeter.where(name: 55555).first.max_time)
     assert_equal(8, ParkingMeter.where(name: 66666).first.price)
   end
 
   test "parse bad details" do
   	parse_test
-    assert_equal(10, ParkingMeter.count)
+    assert_equal(11, ParkingMeter.count)
     assert_equal(0, ParkingMeter.where(name: 77777).first.max_time)
     assert_equal(0, ParkingMeter.where(name: 88888).first.start_time)
     assert_equal(86399, ParkingMeter.where(name: 88888).first.end_time)
@@ -30,7 +41,7 @@ class ParkingMeterTest < ActiveSupport::TestCase
 
   test "parse bad coords" do
   	parse_test
-    assert_equal(10, ParkingMeter.count)
+    assert_equal(11, ParkingMeter.count)
 
     assert_equal(0, ParkingMeter.where(name: 00000).first.lat_lon.lat)
     assert_equal(0, ParkingMeter.where(name: 00000).first.lat_lon.lon)
